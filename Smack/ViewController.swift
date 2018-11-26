d50871b4fb6b7485430dd65b0bbcb8092418e86d
spvagelis <spirou.vagelis@gmail.com>
2018-11-26 12:47:37 +0200

    Starter project

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by vagelis spirou on 26/11/2018.
+//  Copyright © 2018 vagelis spirou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
