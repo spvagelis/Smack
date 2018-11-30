73cae02b5628b6a82cb49b088a8fb099e7703903
spvagelis <spirou.vagelis@gmail.com>
2018-11-30 21:02:02 +0200

    I used Json to login a user

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -14,6 +14,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 let BASE_URL = "https://chattywe.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 // Segues
 
@@ -26,3 +27,9 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+
+let HEADER = [
+    "Content-type": "application/json; charset=utf-8"
+]
